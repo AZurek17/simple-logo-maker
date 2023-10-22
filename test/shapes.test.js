47b18ebff96bf173a831f77f3395856b2ffc01ac
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-22 15:57:09 -0700

    added test folder for jest

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,7 +1,7 @@
 // const Shape = require("./lib/shapes");
-const Triangle = require("./lib/shapes");
-const Circle = require("./lib/shapes");
-const Square = require("./lib/shapes");
+const Triangle = require("../lib/shapes");
+const Circle = require("../lib/shapes");
+const Square = require("../lib/shapes");
 
 // const shape = new Triangle();
 // shape.setColor("blue");
@@ -9,7 +9,7 @@ const Square = require("./lib/shapes");
 
 describe('Triangle', () => {
     describe('' , () => {
-        it('should show a blue triangle', () => {
+        it('should create a SVG file to show a blue triangle', () => {
         const shape = new Triangle();
         shape.setColor("blue");
         expect(shape.render()).toEqual(<polygon points="150 10, 50 145, 250 145" fill="blue"/>)
@@ -19,7 +19,7 @@ describe('Triangle', () => {
 
 describe('Circle', () => {
     describe('' , () => {
-        it('should show a blue circle', () => {
+        it('should create a SVG file to show a blue circle', () => {
         const shape = new Circle();
         shape.setColor("blue");
         expect(shape.render()).toEqual(<circle cx="145" cy="100" r="80" fill="blue"/>)
@@ -29,7 +29,7 @@ describe('Circle', () => {
 
 describe('Square', () => {
     describe('' , () => {
-        it('should show a blue square', () => {
+        it('should create a SVG file to show a blue square', () => {
         const shape = new Square();
         shape.setColor("blue");
         expect(shape.render()).toEqual(<rect x="65" y="50" width="165" height="115" fill="blue"/>)
