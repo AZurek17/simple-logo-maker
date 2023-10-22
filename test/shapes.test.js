48821c71a594ce1570e09c2872fcf8075fff2e14
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-22 16:37:08 -0700

    all tests passing

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,38 +1,31 @@
-// const Shape = require("./lib/shapes");
-const Triangle = require("../lib/shapes");
-const Circle = require("../lib/shapes");
-const Square = require("../lib/shapes");
-
-// const shape = new Triangle();
-// shape.setColor("blue");
-// expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+const {Triangle,Circle,Square} = require("../lib/shapes.js");
 
 describe('Triangle', () => {
-    describe('' , () => {
+    describe('render' , () => {
         it('should create a SVG file to show a blue triangle', () => {
-        const shape = new Triangle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual(<polygon points="150 10, 50 145, 250 145" fill="blue"/>)
-        })
-    })
+            const shape = new Triangle();
+            shape.selectedColor("blue");
+            expect(shape.render()).toEqual(`<polygon points="150 10, 50 145, 250 145" fill="blue"/>`);
+        });
+    });
 });
 
 describe('Circle', () => {
     describe('' , () => {
-        it('should create a SVG file to show a blue circle', () => {
-        const shape = new Circle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual(<circle cx="145" cy="100" r="80" fill="blue"/>)
-        })
-    })
+            it('should create a SVG file to show a blue circle', () => {
+            const shape = new Circle();
+            shape.selectedColor("blue");
+            expect(shape.render()).toEqual(`<circle cx="145" cy="100" r="80" fill="blue"/>`);
+        });
+    });
 });
 
 describe('Square', () => {
     describe('' , () => {
         it('should create a SVG file to show a blue square', () => {
-        const shape = new Square();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual(<rect x="65" y="50" width="165" height="115" fill="blue"/>)
-        })
-    })
+           const shape = new Square();
+           shape.selectedColor("blue");
+           expect(shape.render()).toEqual(`<rect x="65" y="50" width="165" height="115" fill="blue"/>`);
+        });
+    });
 });
